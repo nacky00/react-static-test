dce36b20b70ca7b5ab49086cd7cbaaa5a63cd124
nacky00 <nacky00eule@gmail.com>
2021-09-10 18:06:44 +0900

    update

--- static.config.js
+++ static.config.js
@@ -2,17 +2,6 @@ import path from 'path'
 import axios from 'axios'
 
 export default {
-  paths: {
-    root: process.cwd(), // プロジェクトのルート。よく分からなければ変更しないでください。
-    src: 'src', // ソースフォルダ。index.jsをエントリーポイントとして含む必要あり。
-    temp: 'tmp', // ビルドで発生する公開しないファイルの置き場。
-    dist: 'dist', // 本番用出力フォルダ。
-    devDist: 'tmp/dev-server', // The development scratch directory.
-    public: 'public', // The public directory (files copied to dist during build)
-    assets: 'dist', // The output directory for bundled JS and CSS
-    buildArtifacts: 'artifacts', // The output directory for generated (internal) resources
-  },
-  siteRoot: 'https://relyontrip.com',
   plugins: [
     [
       require.resolve('react-static-plugin-source-filesystem'),
